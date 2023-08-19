c6b0f41fba98b9afe296fc2f4c9ba9db322b98c9
Anhal30 <122851821+Anhal30@users.noreply.github.com>
2023-08-19 12:35:03 +0400

    Update pro.js

--- problem solving/pro.js
+++ problem solving/pro.js
@@ -241,3 +241,31 @@ const s = "women's day";
 const result = countEqualSubstrings(s);
 
 console.log(result);
+
+
+
+
+//Q10:given on array arr[ ] containing positive element A and B are two numbers defining a range .
+//the task is to check if the array contains all elements in the given range ,Input : N=7,A=2,B=5 ,arr[ ]={1,4,5,2,7,8,3}
+
+function isRangeContained(arr, A, B) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < A || arr[i] > B) {
+            return false;
+        }
+    }
+    return true;
+}
+const N = 7;
+const A = 2;
+const B = 5;
+const arr = [1, 4, 5, 2, 7, 8, 3];
+
+
+const result = isRangeContained(arr, A, B);
+
+if (result) {
+    console.log("All elements are within the specified range.");
+} else {
+    console.log("Not all elements are within the specified range.");
+}
